360e9b56c8fb6ea20f3f4fdc631334d7d3de8c8c
Elylelf <elylelf@gmail.com>
2017-11-16 12:21:50 +0100

    eh hop

--- exercice1.php
+++ exercice1.php
@@ -1,1 +1,37 @@
-fejo
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php // Créer les tableaux de bateaux
+    $bateaux = array(
+    "Manureva" => array ( "nbEquipage" => 3, "passager" => 4),
+    "Sirius" => array ( "nbEquipage" => 6, "passager" => 8),
+    "Surcouf" => array ( "nbEquipage" => 8, "passager" => 15)
+    ); ?>
+    <!-- Parcourir les tableaux
+    foreach($bateaux as $lesbateaux => $lequipage) {
+            echo $lesbateaux;
+        foreach($lequipage as $personnes => $valeurs) {
+                echo $personnes.$valeurs;
+        }
+    } -->
+    <h1>Taratatam !</h1>
+    <p>Nous allons afficher les bateaux qui peuvent accepter au moins 10 passagers:</p>
+    <?php // autre Parcourt
+    foreach($bateaux as $lesbateaux => $lequipage) {
+       if($lequipage["passager"] >= 10) {
+            echo $lesbateaux;
+            echo "<br>Equipage : ".$lequipage["nbEquipage"];
+            echo "<br>Passagers : ".$lequipage["passager"]."<br>";
+       }
+    }
+    // ou
+    //$bateaux["Manureva"]["nbEquipage"];
+    ?>
+</body>
+</html>
